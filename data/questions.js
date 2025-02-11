5ade1c63d47b0ee2cd7f710ed2a6a0e30223c7bc
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-02-10 20:14:58 -0600

    Set up question bank and imported it to the quiz script

---
+++ data/questions.js
@@ -0,0 +1,77 @@
+export const questionBank = [
+  {
+    questionId: 1,
+    question: "What is my name?",
+    options: {
+      a: "Bob",
+      b: "Melissa",
+      c: "Mark",
+      d: "Grace",
+    },
+    answer: "c",
+    feedback: {
+      correct: "That's right! My name is Mark.",
+      incorrect: "That is not my name.",
+    },
+  },
+  {
+    questionId: 2,
+    question: "Where are we?",
+    options: {
+      a: "Earth",
+      b: "The United States of America",
+      c: "Here",
+      d: "Greece",
+    },
+    answer: "b",
+    feedback: {
+      correct: "Correct: we are in the US.",
+      incorrect: "You don't know where you are? That's concerning.",
+    },
+  },
+  {
+    questionId: 3,
+    question: "What color is the sky?",
+    options: {
+      a: "Red",
+      b: "It depends on the time of the day!",
+      c: "Blue",
+      d: "Grace",
+    },
+    answer: "c",
+    feedback: {
+      correct: "Very good, the sky is blue!",
+      incorrect: "You should go out more often",
+    },
+  },
+  {
+    questionId: 4,
+    question: "How do you say 'Hello' in Spanish?",
+    options: {
+      a: "Hola",
+      b: "Ciao",
+      c: "?Como ce dice hola en ingles?",
+      d: "Yellow",
+    },
+    answer: "a",
+    feedback: {
+      correct: "!Hola- muy bien!",
+      incorrect: "Keep trying, you'll get there!",
+    },
+  },
+  {
+    questionId: 5,
+    question: "What is my name?",
+    options: {
+      a: "Bob",
+      b: "Melissa",
+      c: "Mark",
+      d: "Grace",
+    },
+    answer: "c",
+    feedback: {
+      correct: "You remembered me!",
+      incorrect: "The sky is always blue.",
+    },
+  },
+];
---
+++ scripts/quiz.js
@@ -0,0 +1,1 @@
+import { questionBank } from "../data/questions";
