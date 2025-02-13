e839e64bdea1f7a1aae996a61f4a10a63cd9035b
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-02-12 22:13:48 -0600

    Created function to render questions
    Created renderQuiz function and worked on SOC by setting up modules between the question bank file, the working quiz file, and the main script file.
    And I am in utter disbelief that everything is working as expected. Feeling both super proud of myself for managing to summon all of this from memory (no AI yet!), and terrified of the next roadblock I'll inevitably face. Here's to learning!

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,1 +1,22 @@
-import { questionBank } from "../data/questions";
+//Function to display questions from the question bank
+export function renderQuiz(dataSet) {
+  const quizInterface = document.querySelector(".quizInterface");
+  dataSet.forEach((question) => {
+    quizInterface.innerHTML = `<h2>${question.question}</h2>
+    <form id="${question.questionId} class="optionsForm">
+    <input type="radio" name="${question.questionId}" id="${question.questionId}a">
+    <label for="${question.questionId}a">a. ${question.options.a}</label>
+    <br>
+    <input type="radio" name="${question.questionId}" id="${question.questionId}b">
+    <label for="${question.questionId}b">b. ${question.options.b}</label>
+    <br>
+    <input type="radio" name="${question.questionId}" id="${question.questionId}c">
+    <label for="${question.questionId}c">c. ${question.options.c}</label>
+    <br>
+    <input type="radio" name="${question.questionId}" id="${question.questionId}d">
+    <label for="${question.questionId}d">d. ${question.options.d}</label>
+    <br>
+    <input type="submit" class="submitAnswer">
+    </form>`;
+  });
+}
---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+import { renderQuiz } from "./quiz.js";
+import { questionBank } from "../data/questions.js";
+renderQuiz(questionBank);
